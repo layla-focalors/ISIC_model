3e7589fc8bf36c3d8cc70a3ee4889a344913f22c
arisia <starlights2283@gmail.com>
2023-11-09 14:46:17 +0900

    add update

--- src/homework.java
+++ src/homework.java
@@ -1,3 +1,24 @@
+class Animal {
+    String name;
+    void setName(String name){
+        this.name = name;
+    }
+}
+class Tiger extends Animal {
+
+}
+class Lion extends Animal {
+
+}
+
+class ZooKeeper {
+    void feed(Tiger tiger){
+        System.out.println("feed apple!");
+    }
+    void feed(Lion lion){
+        System.out.println("feed ");
+    }
+}
 public class homework {
     public static void main(String[] args){
         System.out.println("Hello Layla-focalors");
