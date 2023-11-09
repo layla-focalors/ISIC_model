80370001ad4ae3684ba32954161c0ed8a1ef8a62
arisia <starlights2283@gmail.com>
2023-11-09 14:48:10 +0900

    add folder

--- src/homework.java
+++ src/homework.java
@@ -16,8 +16,10 @@ class ZooKeeper {
         System.out.println("feed apple!");
     }
     void feed(Lion lion){
-        System.out.println("feed ");
+        System.out.println("feed banana!");
     }
+//    문제점 : 악어, 사슴, 토끼, 곰, 다람쥐, 말 등의 동물들이 늘어나면 그때마다
+//    feed 메서드로 추가되는 동물에 맞게 오버로딩 시켜야 함
 }
 public class homework {
     public static void main(String[] args){
